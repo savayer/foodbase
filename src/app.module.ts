85e81b62459966a5e1fef8439f5c2e2377dafc9f
savayer <sava0094@gmail.com>
2025-01-12 21:29:02 +0400

    added docker & mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@database:27017/${process.env.DB_NAME}?authSource=admin`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
