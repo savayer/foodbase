9ac3ad1c14cf8b4492f076776a19c93ae81062a9
savayer <sava0094@gmail.com>
2025-01-18 15:23:50 +0400

    refactored users model, added global prefix

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,14 +1,16 @@
-import * as mongoose from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-});
-
-export interface User extends mongoose.Document {
-  id: string;
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
   name: string;
+
+  @Prop({ required: true })
   email: string;
+
+  @Prop({ required: true })
   password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
